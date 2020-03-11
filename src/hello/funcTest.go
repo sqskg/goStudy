91d31da8678fe6531285d66fb61de73da98e1d9c
shiqingshan <sqskg@163.com>
2020-03-11 15:51:20 +0800

    function test

---
+++ src/hello/funcTest.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main()  {
+	
+	fmt.Println(calculateBill(10,2))
+	var area,perimetr = rectProps(10,20)
+	fmt.Println("area = ",area,";perimeter = ",perimetr)
+	area1, _ := rectProps(10.8, 5.6) // 返回值周长被丢弃
+	fmt.Printf("Area %f ", area1)
+}
+
+func calculateBill(price int,no int) int {
+	return price * no
+}
+
+func rectProps(length,width float64) (float64,float64) {
+	var area = length * width
+	var perimeter = (length + width) * 2
+	return area,perimeter
+}
