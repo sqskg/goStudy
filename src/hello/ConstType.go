d4fb4fb47472f53a9b8f067fc0b2f5bb5917a0d8
shiqingshan <sqskg@163.com>
2020-03-11 15:38:25 +0800

    const type

--- src/hello/ConstType.go
+++ src/hello/ConstType.go
@@ -1,11 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main()  {
+// go 的常量类型用 const 标注
+func main() {
 
 	var a int = 50
-	a = 60;
+	//const a int = 50
+	a = 60
 	fmt.Println(a)
 
+	var b = math.Sqrt(4)
+	//const b1 = math.Sqrt(8);
+
+	fmt.Println(b)
+
+	const hello string = "hello world"
+
+	fmt.Println(hello)
+
+	const trueConst = true
+	type myBool bool
+	var defaultBool = trueConst       // 允许
+	var customBool myBool = trueConst // 允许
+	fmt.Println(defaultBool)
+	fmt.Println(customBool)
+	//defaultBool = customBool // 不允许
+
+	const s = 5
+	var intVar int = s
+	var int32Var int32 = s
+	var float64Var float64 = s
+	var complex64Var complex64 = s
+	fmt.Println("intVar", intVar, "\nint32Var", int32Var, "\nfloat64Var", float64Var, "\ncomplex64Var", complex64Var)
+	{
+		var i = 5
+		var f = 5.6
+		var c = 5 + 6i
+		fmt.Printf("i's type %T, f's type %T, c's type %T", i, f, c)
+	}
 }
