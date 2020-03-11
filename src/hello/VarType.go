b767d09a4cc8f60474d32b517a9ad38ca5d81f4d
shiqingshan <sqskg@163.com>
2020-03-11 10:56:25 +0800

    bool type

--- src/hello/VarType.go
+++ src/hello/VarType.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main()  {
-	a := true
-	b := false
-	fmt.Println("a = ",a,"b =",b)
+	//bool 类型表示一个布尔值，值为 true 或者 false。
+	a := true;
+	b := false;
+	fmt.Println("a = ",a,"b =",b);
+	c := a && b;
+	fmt.Println("c = ",c);
+	d := a || b;
+	fmt.Println("d = ",d);
 }
