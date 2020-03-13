aaae6a26b0e2de16847a3b779b1145fa9b99569a
shiqingshan <sqskg@163.com>
2020-03-13 08:26:48 +0800

    multi slice

--- src/array/arrayTest.go
+++ src/array/arrayTest.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main()  {
 	//var a [2]int
@@ -24,7 +26,14 @@ func main()  {
 	////数组 num 实际上是通过值传递给函数 changeLocal
 	//fmt.Println("after passing to function ", num)
 	//range1()
-	slice();
+	//slice();
+	//valueTransfer()
+	//multiArray();
+	//makeSilce();
+	//extendsCap();
+	//nilTest();
+	//sliceValueTest();
+	multiSlice();
 }
 
 
@@ -47,5 +56,88 @@ func slice()  {
 	a := [5]int{76, 77, 78, 79, 80}
 	var b []int = a[1:4] // creates a slice from a[1] to a[3]
 	fmt.Println(b)
+}
+
+//当数组作为参数传递给函数时，它们是按值传递，而原始数组保持不变
+func valueTransfer()  {
+	num := [...]int{5, 6, 7, 8, 8}
+	fmt.Println("before passing to function ", num)
+	changeLocal(num) //num is passed by value
+	fmt.Println("after passing to function ", num)
+}
+
+func multiArray()  {
+	a := [3][2]string{
+		{"lion", "tiger"},
+		{"cat", "dog"},
+		{"pigeon", "peacock"}, // this comma is necessary. The compiler will complain if you omit this comma
+	}
+	printArray(a)
+	var b [3][2]string
+	b[0][0] = "apple"
+	b[0][1] = "samsung"
+	b[1][0] = "microsoft"
+	b[1][1] = "google"
+	b[2][0] = "AT&T"
+	b[2][1] = "T-Mobile"
+	fmt.Printf("\n")
+	printArray(b)
+}
+func printArray(a [3][2]string)  {
+	for _, v1 := range a  {
+		for _, v2 := range v1{
+			fmt.Printf("%s ",v2)
+		}
+		fmt.Println("")
+	}
+}
+func makeSilce(){
+	i := make([]int,5,5)
+	fmt.Println("len is ",len(i),"cap is ", cap(i))
+	fmt.Println(i)
+}
+
+//如果往一个满了切片里添加数据，它会自动扩容到原理的2倍
+func extendsCap(){
+	car := []string{"Ferrari","Honda", "Ford"}
+	fmt.Println("car's = ",car,"car's len = ", len(car),"car's cap = ", cap(car))
+	car = append(car, "toyota")
+	fmt.Println("car's = ",car,"car's len = ", len(car),"car's cap = ", cap(car))
+}
+
+//zero value of a slice is nil
+func nilTest()  {
+	var names []string;
+	if names == nil {
+		fmt.Println("names = ",names,"names len = ", len(names),"names cap = ", cap(names))
+		names = append(names, "John","Sebastian");
+		fmt.Println("names = ",names,"names len = ", len(names),"names cap = ", cap(names))
+		names = append(names, "jordon")
+		fmt.Println("names = ",names,"names len = ", len(names),"names cap = ", cap(names))
+		names = append(names, "jordon")
+		fmt.Println("names = ",names,"names len = ", len(names),"names cap = ", cap(names))
+	}
+}
+
+func sunElement(number []int)  {
+	for i, _ := range number{
+		number[i] -= 2
+	}
+}
+func sliceValueTest(){
+	nos := []int{9,10,10}
+	fmt.Println("nos = ",nos)
+	sunElement(nos)
+	fmt.Println("nos = ",nos)
+}
+
+func multiSlice()  {
+	pls := [][]string{{"22","23"},{"000"},{"32","33"}}
+	for _,v1 := range pls{
+		for _,v2 := range v1 {
+			fmt.Print(" ",v2)
+		}
+		fmt.Println("")
+	}
 
 }
